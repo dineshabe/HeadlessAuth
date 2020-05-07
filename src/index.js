698cc542a5eb77250079365b98de878c6f5506d7
Dinesh Abraham Thangasamy <dineshabe@yahoo.co.in>
2020-05-06 23:27:41 -0600

    bug fixes

--- src/index.js
+++ src/index.js
@@ -5,19 +5,27 @@ const Code = require('./model/code')
 
 const port = process.env.PORT || 3000
 const app = express()
+
+app.use(express.json())
+
 var getRandomNumber = rn.generator({
     min:  0, 
     max:  999999, 
     integer: true
   })
 
-app.post('/code', (req, res) => {
+app.post('/code', async (req, res) => {
     const generatedCode = getRandomNumber()
     const newCode = new Code({
         code: generatedCode,
         requestingDevice: req.body.device
     })
-    res.status(201).send({code: generatedCode})
+    try {
+        await newCode.save()
+        res.status(201).send({code: generatedCode})
+    } catch (e) {
+        res.status(400).send(e)
+    }
 })
 
 app.post('/code/:id', async (req, res) => {
@@ -26,6 +34,7 @@ app.post('/code/:id', async (req, res) => {
         const code = await Code.findOne({code: receivedCode})
         code.responderDevice = req.body.device
         code.accessToken = req.body.accessToken
+        await code.save()
         res.status(200).send({device: code.requestingDevice})
     } catch (e) {
         res.status(401).send(e)
@@ -37,7 +46,7 @@ app.get('/code/:id', async (req, res) => {
     try {
         const code = await Code.findOne({code: receivedCode})
         if (code.accessToken) {
-            res.status(200).send({accessToken, device: responderDevice})
+            res.status(200).send({accessToken: code.accessToken, device: code.responderDevice})
         } else {
             res.status(503).send({message: 'Just a little more time my child'})
         }
